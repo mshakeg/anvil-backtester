e21bfb9bfd73c5a303932fcc1389484fce56fb79
Mo Shaikjee <shaikjeemohammed@gmail.com>
2024-02-07 21:20:26 +0200

    log tps analytics

--- shell/anvil.sh
+++ shell/anvil.sh
@@ -1,4 +1,4 @@
-anvil --prune-history --transaction-block-keeper 16 --timestamp 1619820000 --order fifo --code-size-limit 4294967296 -m "test test test test test test test test test test test junk" --gas-limit 100000000000 --gas-price 0 --base-fee 0
+anvil --prune-history --transaction-block-keeper 4 --timestamp 1619820000 --order fifo --code-size-limit 4294967296 -m "test test test test test test test test test test test junk" --gas-limit 100000000000 --gas-price 0 --base-fee 0
 
 # UniswapV3Factory was deployed to the Ethereum Mainnet at Block: 12369621 i.e.  (May-04-2021 07:27:00 PM +UTC)
 # So genesis is set to 1 May 2021 00:00
